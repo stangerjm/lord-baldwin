bafe593d3705ebf0c29a60bec6d12b656a826eca
stangerjm <jamesstanger987@gmail.com>
2022-12-29 14:46:21 -0800

    Revamped the writing page and added new downloads view

---
+++ src/views/baldwin-downloads.vue
@@ -0,0 +1,36 @@
+<template>
+  <article>
+    <img />
+    <img />
+    <section class="baldwin-downloads--titleContainer">
+      <h3 class="baldwin-downloads--title">For a Limited Time</h3>
+      <h3><span class="baldwin-downloads--emphasis">Free</span> download of</h3>
+      <h3>Lord Baldwin's</h3>
+      <h3>From the Lost Letters Sent</h3>
+      <h3>Memoirs from an Invisible Songwriter</h3>
+      <h3>all four books</h3>
+      <h3>in PDF format</h3>
+    </section>
+    <section class="baldwin-downloads--poster">
+      <img src="../assets/images/ResilientPoster.jpg" />
+      <h3>Free poster download Lord Baldwin's "Resilient" in PDF format</h3>
+    </section>
+    <img />
+    <img />
+    <section class="baldwin-downloads--resilientMusical">
+      <img src="../assets/images/ResilientTheMusical.jpg" />
+      <h3>"Resilient - The Musical" web-based episodes about the houseless</h3>
+      <h3>Musical play coincides with the music and songs from the album "Resilient"</h3>
+    </section>
+    <img />
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'baldwin-downloads',
+};
+</script>
+
+<style>
+</style>
--- src/views/baldwin-writing.vue
+++ src/views/baldwin-writing.vue
@@ -1,70 +1,217 @@
 <template>
-  <article>
-    <h1>Writings of Lord Baldwin</h1>
-    <section>
-      <div>
-        <h2>Heads or Tales from the Summer of Love</h2>
-        <img src="../assets/images/Heads.jpg" alt="Heads or Tales from the Summer of Love">
-        <a href="../content/Heads_excerpt.pdf">Click here to read a sample chapter</a>
-      </div>
-      <p>
-        Set in the counter-cultural generation backdrop of 1966-1967, Heads, or Tales from
-        the Summer of Love chronicles the struggles and sometimes funny situations of Reuben,
-        a 16-year-old, whose life, after moving to Portland, Oregon, in the summer of 1966
-        with his mother, sister and two brothers, is almost perfect. As school starts, he is
-        actively playing on the Cleveland High School’s football team, he has a girlfriend, he
-        is doing well in all his studies, he is popular with everyone, and for the first time
-        in his life, Reuben has his own bedroom. But for Reuben, the good times come to a sudden
-        halt when his mother unexpectedly reconciles with her estranged husband, Senior. Soon,
-        they are all heading to the Los Angeles area into a house in suburbia Glendora, where
-        Reuben struggles to adjust to the move. This is made all the worse as, shortly after they
-        arrive, Reuben and his brother Chad have run-ins with their stepfather, Senior, who grows
-        less tolerant of Reuben and Chad, thrusting punishments and restrictions on them that last
-        into the summer. As things at home get progressively worse, Reuben, through a set of
-        mistakes and misunderstandings, runs into an assortment of problematic circumstances that
-        inevitably finds him homeless in Glendora for the entire summer of 1967. Heads, or Tales
-        from the Summer of Love is an eclectic collection of intertwined stories that follows
-        the varied paths and episodes of Reuben and his friends.
-        <a href="#">Click here to order</a>
-      </p>
+  <header>
+    <h1 class="baldwin-writing--title">Lord Baldwin Writings</h1>
+    <h2 class="baldwin-writing--sub">Now available in book stores everywhere</h2>
+  </header>
+  <article class="baldwin-writing">
+    <h3 class="baldwin-writing--ants">
+      <span ref="antsRow1" class="baldwin-writing--ants__row1">Stepping between</span>
+      <span ref="antsRow2" class="baldwin-writing--ants__row2">the ants</span>
+    </h3>
+    <img class="baldwin-writing--ants1" src="../assets/images/SteppingBetweenTheAnts1.jpg" />
+    <img class="baldwin-writing--ants2" src="../assets/images/SteppingBetweenTheAnts2.png" />
+    <img class="baldwin-writing--ants3" src="../assets/images/SteppingBetweenTheAnts3.jpg" />
+    <img class="baldwin-writing--ants4" src="../assets/images/SteppingBetweenTheAnts4.png" />
+    <section class="baldwin-writing--resilient">
+      <h3>Resilient</h3>
+      <img src="../assets/images/ResilientTheMusical.jpg" />
     </section>
-    <section>
-      <div>
-        <h3>You may also like...</h3>
-        <h2>Stepping Between the Ants</h2>
-        <img src="../assets/images/Stepping.jpg" alt="Stepping Between the Ants">
-        <a href="../content/Stepping_excerpt.pdf">Click here to read a sample chapter</a>
-      </div>
-      <p>
-        The winter of 1962-63 in Iron Mountain, Michigan, sees a record 159 inches of snow and the
-        winter is in the top 25 coldest years in the past 116 years; so cold that Lake Michigan
-        freezes over. To make matters worse, Fisher’s mother has problems with her husband,
-        Fisher’s stepfather, Senior, who has issues with alcohol and stress-anger management.
-        In February of 1963, thirteen-year-old Fisher Bean, along with his mother, two brothers,
-        Nicky and Luke, and his little sister Rachel, all get on a bus to run away to the west
-        coast. This story begins with the bus trip to Portland, Oregon, and their move into Grandma
-        Scarbrough’s house. As Fisher and Chad attempt to make friends with the kids in his new
-        neighborhood and to fit into their new surroundings, Fisher discovers that finding good kids
-        to be friends is not as easy as it would seem. He gets involved with the wrong crowd and
-        gets in trouble. His grandmother seems to continually find fault with him, and his brother
-        Chad has problems with bullies at school. After making many adjustments, and in spite of
-        Fisher’s hopes to continue his trail to become an Eagle Scout with the Boy Scouts of
-        America, he runs into more problems that lead him down an unexpected path. This is a
-        coming-of-age story about a boy who strives to fit in, but at the same time, loves Scouting
-        and is reminded of its principals and ideals. In his process of trying to make friends and
-        within his search to figure out who and what he is, he finds instead, others that will seek
-        him out and choose him to be the good friend that they need.
-        <a href="#">Click here to order</a>
-      </p>
-    </section>
+    <h3 class="baldwin-writing--letters">
+      <span ref="lettersRow1" class="baldwin-writing--letters__row1">From the Lost</span>
+      <span ref="lettersRow2" class="baldwin-writing--letters__row2">Letters Sent</span>
+    </h3>
+    <img class="baldwin-writing--letters1" src="../assets/images/FromTheLostLettersSent1.jpg" />
+    <img class="baldwin-writing--letters2" src="../assets/images/FromTheLostLettersSent2.jpg" />
+    <img class="baldwin-writing--letters3" src="../assets/images/FromTheLostLettersSent3.jpg" />
+    <img class="baldwin-writing--letters4" src="../assets/images/FromTheLostLettersSent4.jpg" />
   </article>
 </template>
 
-<script>
-export default {
-  name: 'baldwin-writing',
-};
+<script setup>
+import { ref, onMounted } from 'vue';
+import CircleType from 'circletype';
+
+const antsRow1 = ref();
+const antsRow2 = ref();
+const lettersRow1 = ref();
+const lettersRow2 = ref();
+
+onMounted(() => {
+  new CircleType(antsRow1.value).radius(384);
+  new CircleType(antsRow2.value).radius(384);
+  new CircleType(lettersRow1.value).radius(800);
+  new CircleType(lettersRow2.value).radius(800);
+});
 </script>
 
-<style>
+<style lang="scss">
+  .baldwin-writing {
+    display: grid;
+    margin: 0 auto;
+    gap: 1rem;
+    justify-content: center;
+    align-items: center;
+    grid-template-columns: repeat(5, min-content);
+    grid-template-areas:
+      "ants ants . letters letters"
+      "ants1 ants2 resilient letters1 letters2"
+      "ants3 ants4 resilient letters3 letters4";
+  }
+
+  .baldwin-writing--title {
+    text-align: center;
+    font-size: 2.5rem;
+    text-shadow: 3px 3px 1px #ac2652; 
+    margin: 0;
+    margin-top: 1rem;
+  }
+
+  .baldwin-writing--sub {
+    text-align: center;
+    color: #dd7a31;
+    font-size: 3.5rem;
+    text-shadow: 3px 3px 1px #0200e0; 
+    margin: 0;
+  }
+
+  .baldwin-writing--ants {
+    font-size: 2rem;
+    grid-area: ants;
+    text-align: center;
+    color: #2f9afb;
+    text-shadow: 3px 3px 1px #5d025c; 
+    margin: 0;
+  }
+
+  .baldwin-writing--ants1 {
+    grid-area: ants1;
+  }
+
+  .baldwin-writing--ants2 {
+    grid-area: ants2;
+  }
+
+  .baldwin-writing--ants3 {
+    grid-area: ants3;
+  }
+
+  .baldwin-writing--ants4 {
+    grid-area: ants4;
+  }
+
+  .baldwin-writing--resilient {
+    grid-area: resilient;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: #74a279;
+    font-size: 2rem;
+    text-shadow: 3px 3px 1px #6c389a; 
+  }
+
+  .baldwin-writing--letters {
+    grid-area: letters;
+    text-align: center;
+    font-size: 2rem;
+    color: #fed968;
+    text-shadow: -2px 2px 1px #4c889c; 
+    margin: 0;
+  }
+
+  .baldwin-writing--ants__row2 {
+    position: absolute;
+    transform: translateY(-1.5rem);
+  }
+
+  .baldwin-writing--letters1 {
+    grid-area: letters1;
+  }
+
+  .baldwin-writing--letters2 {
+    grid-area: letters2;
+  }
+
+  .baldwin-writing--letters3 {
+    grid-area: letters3;
+  }
+
+  .baldwin-writing--letters4 {
+    grid-area: letters4;
+  }
+
+  .baldwin-writing img {
+    height: 15rem;
+    justify-self: center;
+    overflow: hidden;
+  }
+
+  @media screen and (max-width: 999px) {
+    .baldwin-writing {
+      width: 100%;
+      grid-template-columns: repeat(2, 1fr);
+      gap: 0.5rem;
+      grid-template-areas:
+        "ants ants"
+        "ants1 ants2"
+        "ants3 ants4"
+        "resilient resilient"
+        "letters letters"
+        "letters1 letters2"
+        "letters3 letters4";
+
+      .baldwin-writing--title,
+      .baldwin-writing--sub,
+      .baldwin-writing--ants,
+      .baldwin-writing--resilient,
+      .baldwin-writing--letters {
+        font-size: 1.5rem;
+      }
+
+      .baldwin-writing--ants {
+        margin-bottom: 0.5rem;
+      }
+
+      .baldwin-writing--ants__row2 {
+        position: absolute;
+        transform: translateY(-1rem);
+      }
+
+      img {
+        width: 10rem;
+      }
+
+      .baldwin-writing--ants1 {
+        justify-self: end;
+      }
+
+      .baldwin-writing--ants2 {
+        justify-self: start;
+      }
+
+      .baldwin-writing--ants3 {
+        justify-self: end;
+      }
+
+      .baldwin-writing--ants4 {
+        justify-self: start;
+      }
+
+      .baldwin-writing--letters1 {
+        justify-self: end;
+      }
+
+      .baldwin-writing--letters2 {
+        justify-self: start;
+      }
+
+      .baldwin-writing--letters3 {
+        justify-self: end;
+      }
+
+      .baldwin-writing--letters4 {
+        justify-self: start;
+      }
+    }
+  }
 </style>
